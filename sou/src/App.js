582605cafebd35e3df7ed1d4493f78a1bd3d8fb0
janmejay25 <janmjaypandya@gmail.com>
2025-06-16 20:54:40 +0530

    Route paging added

--- sou/src/App.js
+++ sou/src/App.js
@@ -1,31 +1,31 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Teachers from './Teachers.jsx'
+import Home from './Home.jsx'
+import Holidays from './Holidays.jsx'
+import Nopage from './Nopage.jsx'
+
+
+
+
 const App = () => {
 
   return (
-    <>
+  
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/teachers" element={<Teachers />} />
+      <Route path="/holidays" element={<Holidays />} />
+      <Route path="*" element={<Nopage/>} />
+    </Routes>
+    </BrowserRouter>
     
-    
-    {/* inline css use */}
-     {/* <h1 Style="color:blue">Janmejay Pandya</h1> */}
-  {/* <h1 style={
-    {
-      color: 'blue',
-    }
-  }>Janmejay Pandya</h1> */}
-  <nav>
-    <ul Style="display:flex; list-style:none; justify-content:center; background-color:lightblue; gap:20px; padding:10px; margin:0;">
-      <li ><a Style="text-decoration:none" href="/">Home</a></li>
-      <li><a Style="text-decoration:none " href="/about">About</a></li>
-      <li><a Style="text-decoration:none " href="/contact">Contact</a></li>
-      <li><a Style="text-decoration:none " href="/services">Services</a></li>
-
-    </ul>
-  </nav>
-    </>
+  
+  
 
 
   )
 }
 export default App
-// This code defines a simple React component named `App` that renders an `<h1>` element with the text "helloworld".
 
 
