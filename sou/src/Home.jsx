0f9da07e87c6d140cb5647f2c346c785c830adaf
janmejay25 <janmjaypandya@gmail.com>
2025-06-16 20:52:36 +0530

    main page

---
+++ sou/src/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero.jsx";
+import Footer from "./footer.jsx";
+
+const Home = () => {
+    return (
+        <>
+        <Navbar />
+        <Hero />
+        <Footer />
+        </>
+    );
+}
+export default Home;
