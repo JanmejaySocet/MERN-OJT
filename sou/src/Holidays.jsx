efc0f8acf69e46c48ecab15fd6dc34e1afaf745d
janmejay25 <janmjaypandya@gmail.com>
2025-06-16 20:52:13 +0530

    Holiday page

---
+++ sou/src/Holidays.jsx
@@ -0,0 +1,14 @@
+import Navbar from './Navbar'
+import Footer from './footer.jsx'
+
+const Holidays = () => {
+    return (
+        <>  
+            <Navbar />
+            <h1>Holidays</h1>
+            <p>List of teachers will be displayed here.</p>
+            <Footer />
+        </>
+    );
+}
+export default Holidays;
