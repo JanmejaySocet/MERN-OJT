adec57e7c17dd365b9d2e5ab100c25206a01823f
janmejay25 <janmjaypandya@gmail.com>
2025-06-16 20:54:03 +0530

    Teacher comp

---
+++ sou/src/Teachers.jsx
@@ -0,0 +1,15 @@
+import Navbar from './Navbar'
+import Footer from './footer.jsx'
+
+
+const Teachers = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Teachers</h1>
+            <p>List of teachers will be displayed here.</p>
+            <Footer />
+        </>
+    );
+}
+export default Teachers;
