ba188f0d775c7390683af16e5fa7f6403b01e03e
janmejay25 <janmjaypandya@gmail.com>
2025-06-17 21:20:02 +0530

    page map added

--- sou/src/Teachers.jsx
+++ sou/src/Teachers.jsx
@@ -2,12 +2,23 @@ import Navbar from './Navbar'
 import Footer from './footer.jsx'
 
 
+
 const Teachers = () => {
+    const teachers = [
+        {name: "John Doe", subject: "Mathematics"}, 
+        {name: "Jane Smith", subject: "Science"},
+        {name: "Emily Johnson", subject: "English"}];
+        
     return (
-        <>
+        <>  
             <Navbar />
             <h1>Teachers</h1>
             <p>List of teachers will be displayed here.</p>
+            <h1>{teachers.map((teacher, index) => (
+                    <li key={index}>
+                        {teacher.name} - {teacher.subject}
+                    </li>
+                ))}</h1>
             <Footer />
         </>
     );
